a38b468b4da9887caf6d1e79cf2e460167fa3348
Martin-Macmini <martinn.henriquez@gmail.com>
2021-06-06 11:49:39 -0500

    Primer prototype e implementacion de fechas

--- js/app.js
+++ js/app.js
@@ -1,1 +1,30 @@
 
+// Constructores
+function Seguro(marca, year, tipo) {
+    
+}
+function UI() {}
+
+// Llenar las opciones del año
+UI.prototype.llenarOpciones = () => {    
+    const max = new Date().getFullYear(),
+            min = max - 20;
+    const selecYear = document.querySelector('#year');
+
+    for (let i = max; i > min; i--) {
+        let option = document.createElement('option');
+        option.value = i;
+        option.textContent = i;
+        selecYear.appendChild(option);        
+    }
+}
+
+
+// Instanciar UI
+const ui = new UI();
+
+
+document.addEventListener('DOMContentLoaded', () => {    
+    ui.llenarOpciones(); // Llena el select con los años..
+});
+
